bd5164efaf0208dee2a4cb0c5b2a62ef394fa1f2
Denys Martyniuk <146194242+morpheus5358@users.noreply.github.com>
2023-11-08 11:47:27 +0200

    Add project files.

---
+++ PR4.5.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, y, R;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "R = "; cin >> R;
+	cout << "y = "; cin >> y;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		if ((y <= R - sqrt(pow(R, 2) - pow((x + R), 2)) && y >= 0) ||
+			(y >= -sqrt(pow(R, 2) - pow(x, 2)) && y <= 0))
+			cout << "yes" << endl;
+		else
+			cout << "no" << endl;
+
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
