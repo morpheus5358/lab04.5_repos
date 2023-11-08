9d8575fa88bd3123adbde0de74ad5f7ce14fadcf
morpheus5358 <146194242+morpheus5358@users.noreply.github.com>
2023-11-08 12:04:04 +0200

    Update PR4.5.cpp

--- PR4.5.cpp
+++ PR4.5.cpp
@@ -1,34 +1,38 @@
 #include <iostream>
 #include <iomanip>
-#include <cmath>
+#include <time.h>
 using namespace std;
 int main()
 {
-	double x, xp, xk, dx, y, R;
-	cout << "xp = "; cin >> xp;
-	cout << "xk = "; cin >> xk;
-	cout << "dx = "; cin >> dx;
+	double x, y, R;
 	cout << "R = "; cin >> R;
-	cout << "y = "; cin >> y;
-	cout << fixed;
-	cout << "---------------------------" << endl;
-	cout << "|" << setw(5) << "x" << " |"
-		<< setw(7) << "y" << " |" << endl;
-	cout << "---------------------------" << endl;
-	x = xp;
-	while (x <= xk)
+	srand((unsigned)time(NULL));
+	for (int i = 0; i < 10; i++)
 	{
+		cout << "x = "; cin >> x;
+		cout << "y = "; cin >> y;
+		
 		if ((y <= R - sqrt(pow(R, 2) - pow((x + R), 2)) && y >= 0) ||
 			(y >= -sqrt(pow(R, 2) - pow(x, 2)) && y <= 0))
 			cout << "yes" << endl;
 		else
 			cout << "no" << endl;
 
-		cout << "|" << setw(7) << setprecision(2) << x
-			<< " |" << setw(10) << setprecision(3) << y
-			<< " |" << endl;
-		x += dx;
 	}
-	cout << "---------------------------" << endl;
+	cout << endl << fixed;
+
+		for (int i = 0; i < 10; i++)
+		{
+			x = 6. * rand() / RAND_MAX - 3;
+			y = 6. * rand() / RAND_MAX - 3;
+			
+			if ((y <= R - sqrt(pow(R, 2) - pow((x + R), 2)) && y >= 0) ||
+				(y >= -sqrt(pow(R, 2) - pow(x, 2)) && y <= 0))
+				cout << setw(8) << setprecision(4) << x << " "
+				<< setw(8) << setprecision(4) << y << " " << "yes" << endl;
+			else
+				cout << setw(8) << setprecision(4) << x << " "
+				<< setw(8) << setprecision(4) << y << " " << "no" << endl;
+		}
 	return 0;
 }
